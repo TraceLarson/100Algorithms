ba836da21b5910177fa2bcd894edf94df7e53810
Trace Larson <trace.larson4917@gmail.com>
2018-12-28 19:39:15 -0600

    solved addBorder

--- addBorder/addBorder.ts
+++ addBorder/addBorder.ts
@@ -1,5 +1,18 @@
 function addBorder(picture: string[]): string[] {
+  let asterisk = "*";
+  let asterisks = "";
+  let asterisksLength = picture[0].length + 2;
 
+  for (let i = 0; i < asterisksLength; i++) {
+    asterisks = asterisks + asterisk;
+  }
+
+  for (let i = 0; i < picture.length; i++) {
+    picture[i] = `${asterisk}${picture[i]}${asterisk}`;
+  }
+  picture.push(asterisks);
+  picture.unshift(asterisks);
+  return picture;
 }
 
-// console.log(addBorder(["abc", "ded"]));
+console.log(addBorder(["abc", "ded"]));
