d0762c7fc0e1c8e1ebe6a1d51b5134970020a7cf
Trace Larson <trace.larson4917@gmail.com>
2018-12-28 18:54:04 -0600

    finished absoluteValuesSumMinimization

---
+++ example.ts
@@ -0,0 +1,7 @@
+function add(num1: number, num2: number): number {
+  // const example: any[] = ['', 0, true]
+  // const example: number[] = [0, 1, 2];
+  // const example: number[][] = [[1,3,2]];
+
+  return num1 + num2;
+}
