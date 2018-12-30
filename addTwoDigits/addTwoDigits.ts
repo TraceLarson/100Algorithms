6eff26616fb6022ccc7e972072df4717311878d3
Trace Larson <trace.larson4917@gmail.com>
2018-12-29 20:06:18 -0600

    solved adjacentElementsProduct

--- addTwoDigits/addTwoDigits.ts
+++ addTwoDigits/addTwoDigits.ts
@@ -1,5 +1,9 @@
 function addTwoDigits(n: any): number {
+  let total = 0;
+  n = n.toString();
+  n.split();
 
+  return parseInt(n[0]) + parseInt(n[1]);
 }
 
-// console.log(addTwoDigits(29));
+console.log(addTwoDigits(29));
--- adjacentElementsProduct/adjacentElementsProduct.ts
+++ adjacentElementsProduct/adjacentElementsProduct.ts
@@ -1,5 +1,25 @@
-function adjacentElementsProduct(inputArray: number[]): number {
+function adjacentElementsProduct(inputArray: number[]): any {
+  let firstArray = [];
+  let secondArray = [];
+  let finalArray = [];
+  let highestValue = 0;
 
+  for (let i = 0; i < inputArray.length; i++) {
+    if (i % 2 === 0) {
+      firstArray.push(inputArray[i]);
+    } else {
+      secondArray.push(inputArray[i]);
+    }
+  }
+  for (let i = 0; i < secondArray.length; i++) {
+    finalArray.push(firstArray[i] * secondArray[i]);
+    if (finalArray[i] > highestValue) {
+      highestValue = finalArray[i];
+    }
+  }
+
+  return highestValue;
 }
 
 console.log(adjacentElementsProduct([3, 6, -2, -5, 7, 3]));
+console.log(adjacentElementsProduct([7, 2, -3, -10, 7, 0]));
