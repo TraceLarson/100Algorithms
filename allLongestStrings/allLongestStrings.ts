db5f432c674609545e8afac960b681fe7048a72e
Trace Larson <trace.larson4917@gmail.com>
2018-12-30 11:33:50 -0600

    solved allLongestStrings

--- allLongestStrings/allLongestStrings.ts
+++ allLongestStrings/allLongestStrings.ts
@@ -1,5 +1,18 @@
 function allLongestStrings(inputArray: string[]): string[] {
+  let largestStringLength = 0;
+  let largestStringArray = [];
+  for (let i = 0; i < inputArray.length; i++) {
+    if (inputArray[i].length > largestStringLength)
+      largestStringLength = inputArray[i].length;
+  }
 
+  inputArray.forEach(string => {
+    string.length === largestStringLength
+      ? largestStringArray.push(string)
+      : "";
+  });
+
+  return largestStringArray;
 }
 
 console.log(allLongestStrings(["aba", "aa", "ad", "vcd", "aba"]));
