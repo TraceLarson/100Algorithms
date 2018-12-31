f2bad85bbe3c8a3ce6a143195696a7c1acc58089
Trace Larson <trace.larson4917@gmail.com>
2018-12-30 23:41:51 -0600

    solved almostIncreasingSequence

--- almostIncreasingSequence/almostIncreasingSequence.ts
+++ almostIncreasingSequence/almostIncreasingSequence.ts
@@ -1,6 +1,20 @@
 function almostIncreasingSequence(sequence: number[]): boolean {
+  let guideNumber = 0;
+  let removedAmount = 0;
+  let isSequential = true;
 
+  for (let i = 0; i < sequence.length + 1; i++) {
+    if (sequence[i] > guideNumber && removedAmount < 2) {
+      guideNumber = sequence[i];
+    } else if (sequence[i] < guideNumber && removedAmount < 2) {
+      removedAmount += 1;
+    } else if (removedAmount > 1) {
+      isSequential = false;
+    }
+  }
+
+  return isSequential;
 }
 
-// console.log(almostIncreasingSequence([1, 3, 2, 1])) 
-// console.log(almostIncreasingSequence([1, 3, 2])) 
+console.log(almostIncreasingSequence([1, 3, 2, 1]));
+console.log(almostIncreasingSequence([1, 3, 2]));
